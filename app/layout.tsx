602538aef6a5e331b653e72c4187513e0cdc11f4
juancelot-plusultra <johnphilipgallana@gmail.com>
2026-02-09 21:18:24 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,8 @@
 import "./globals.css"
-import type { Metadata } from "next"
 
-export const metadata: Metadata = {
+export const metadata = {
   title: "BearFitPH",
-  description: "Gym management app",
+  description: "BearFitPH dashboard",
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
