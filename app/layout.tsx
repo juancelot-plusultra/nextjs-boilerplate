ba723d2dead596d0ab3f4fc5261eab93a32b494b
juancelot-plusultra <johnphilipgallana@gmail.com>
2026-02-09 15:39:36 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" className="dark" suppressHydrationWarning>
       <body className="min-h-screen bg-background text-foreground antialiased">
         {children}
       </body>
