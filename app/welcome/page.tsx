f154fa29bf93e8d561479c8ff06337e1587e9acb
juancelot-plusultra <johnphilipgallana@gmail.com>
2026-02-10 21:05:14 +0800

    Update page.tsx

--- app/welcome/page.tsx
+++ app/welcome/page.tsx
@@ -327,7 +327,6 @@ export default function WelcomePage() {
       </div>
     </div>
   </>
-)}
 
       {/* Bottom controls */}
       <div className="absolute bottom-6 inset-x-0 px-6 flex justify-between items-center text-white">
