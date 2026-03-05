a07d509bc3286d9f6e67daa9ea1ff1dd7176972a
juancelot-plusultra <johnphilipgallana@gmail.com>
2026-03-05 08:19:02 +0800

    Update page.tsx

--- app/welcome/page.tsx
+++ app/welcome/page.tsx
@@ -3,7 +3,7 @@
 import Image from "next/image";
 import { useEffect, useMemo, useRef, useState } from "react";
 import { signIn } from '../lib/supabaseActions'; // Import the Supabase auth function
-import LoginForm from '../components/LoginForm.tsx';  // Correct path
+import LoginForm from '../components/LoginForm';  // Correct path to the LoginForm component
 
 type Slide = {
   key: string;
