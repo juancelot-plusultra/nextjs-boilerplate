8f1c02f11e0b4e49476699a318980b266cacf401
juancelot-plusultra <johnphilipgallana@gmail.com>
2026-03-13 15:57:53 +0800

    Update member-dashboard-view.tsx

--- components/bearfit/views/member-dashboard-view.tsx
+++ components/bearfit/views/member-dashboard-view.tsx
@@ -5,6 +5,7 @@ import MemberScheduleView from "@/components/bearfit/member/member-schedule-view
 import MemberPaymentView from "@/components/bearfit/member/member-payment-view"
 import MemberProfileView from "@/components/bearfit/member/member-profile-view"
 import MemberMoreView from "@/components/bearfit/member/member-more-view"
+import MemberBottomNav from "@/components/bearfit/member/member-bottom-nav"
 
 type Props = {
   activeRole: string
@@ -12,21 +13,46 @@ type Props = {
   setActiveTab: (tab: string) => void
 }
 
-export default function MemberDashboardView({ activeRole, activeTab }: Props) {
+export default function MemberDashboardView({
+  activeRole,
+  activeTab,
+  setActiveTab,
+}: Props) {
+
   if (activeRole !== "Member") return null
 
-  switch (activeTab) {
-    case "home":
-      return <MemberHomeView />
-    case "schedule":
-      return <MemberScheduleView />
-    case "payment":
-      return <MemberPaymentView />
-    case "profile":
-      return <MemberProfileView />
-    case "more":
-      return <MemberMoreView />
-    default:
-      return <MemberHomeView />
+  const renderTab = () => {
+    switch (activeTab) {
+      case "home":
+        return <MemberHomeView />
+
+      case "schedule":
+        return <MemberScheduleView />
+
+      case "payment":
+        return <MemberPaymentView />
+
+      case "profile":
+        return <MemberProfileView />
+
+      case "more":
+        return <MemberMoreView />
+
+      default:
+        return <MemberHomeView />
+    }
   }
+
+  return (
+    <div className="pb-20">
+
+      {renderTab()}
+
+      <MemberBottomNav
+        activeTab={activeTab}
+        setActiveTab={setActiveTab}
+      />
+
+    </div>
+  )
 }
