049a6c5f12eb356422ad8e3f872c56366e36d52e
juancelot-plusultra <johnphilipgallana@gmail.com>
2026-03-13 16:36:50 +0800

    Update member-schedule-view.tsx

--- components/bearfit/member/member-schedule-view.tsx
+++ components/bearfit/member/member-schedule-view.tsx
@@ -1,7 +1,11 @@
 "use client"
 
-import SchedulePage from "@/components/bearfit/schedule-page"
+import { PromoBanner } from "@/components/bearfit/promo-banner"
 
 export default function MemberScheduleView() {
-  return <SchedulePage />
+  return (
+    <div className="space-y-5">
+      <PromoBanner />
+    </div>
+  )
 }
